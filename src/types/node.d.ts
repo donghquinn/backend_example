2ea60fdd53c36562d70cfc4939c498e9ac358ef6
dong <ehdgus1524@gmail.com>
2022-10-04 23:51:51 +0900

    [ADD] example env & node.d.ts for declare Global env

---
+++ src/types/node.d.ts
@@ -0,0 +1,16 @@
+declare global {
+    namespace ProcessEnv {
+        interface NodeJs {
+            // 어플리케이션 관련
+            APP_PORT: number;
+            NODE_ENV: string;
+
+            // MYSQL 관련
+            MYSQL_USER: string;
+            MYSQL_PASSWD: string;
+            MYSQL_HOST: string;
+            MYSQL_PORT: string;
+            MYSQL_NAME: string;
+        }
+    }
+}
